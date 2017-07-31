c8418bdf7bbf66fc41ccabcf9e03118e2632fb78
Kagerjay <thechinglisher@gmail.com>
2017-07-31 16:33:42 -0400

    Create 1-MultiplesOf3and5.js

---
+++ 1-MultiplesOf3and5.js
@@ -0,0 +1,15 @@
+let sum = 0;
+
+for (let i = 0; i<1000; i++) {
+  
+  if (i%15===0) {
+    sum+=i;
+  } else if(i%5===0){
+    sum+=i;
+  } else if(i%3===0){
+    sum+=i;
+  } else {
+  }
+}
+
+console.log(sum); //233168
